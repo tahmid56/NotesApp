4062c07097e93b8770770042ccabdbc7423a70d2
Tahmid Akter <tahmidakter56@gmail.com>
2023-03-07 18:18:09 +0600

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Tahmid Akter on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
